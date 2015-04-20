7964a52267c509ba958350d3ea99102537df3bc6
bysong <bysong@tudou.com>
2015-04-20 11:27:56 +0800

    getPackageArchiveInfo

--- sample/src/org/bbs/demo/apkparser/MainActivity.java
+++ sample/src/org/bbs/demo/apkparser/MainActivity.java
@@ -4,12 +4,17 @@ import org.bbs.apkparser.ApkManifestParser;
 import org.bbs.apkparser.PackageInfoX;
 import org.bbs.apkparser.demo.R;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
+
+	private static final String TAG = MainActivity.class.getSimpleName();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,18 @@ public class MainActivity extends ActionBarActivity {
 		
 		String apkFile = getApplicationInfo().publicSourceDir;
 		ApkManifestParser.parseAPk(this, apkFile, true, true).dump(PackageInfoX.DUMP_ALL);
+		
+		int flags = PackageManager.GET_ACTIVITIES|PackageManager.GET_CONFIGURATIONS
+				|PackageManager.GET_DISABLED_COMPONENTS| PackageManager.GET_DISABLED_UNTIL_USED_COMPONENTS
+				|PackageManager.GET_GIDS| PackageManager.GET_INSTRUMENTATION 
+				|PackageManager.GET_INTENT_FILTERS | PackageManager.GET_META_DATA
+				|PackageManager.GET_PERMISSIONS | PackageManager.GET_PROVIDERS
+				|PackageManager.GET_PROVIDERS | PackageManager.GET_RECEIVERS
+				|PackageManager.GET_RESOLVED_FILTER | PackageManager.GET_SERVICES
+				|PackageManager.GET_SHARED_LIBRARY_FILES| PackageManager.GET_SERVICES
+				|PackageManager.GET_UNINSTALLED_PACKAGES | PackageManager.GET_URI_PERMISSION_PATTERNS;
+		PackageInfo pinfo = getPackageManager().getPackageArchiveInfo(apkFile, flags);
+		Log.d(TAG, "pinfo: " + pinfo);
 	}
 
 	@Override
